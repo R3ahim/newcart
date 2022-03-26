64150995d9f33a58565f1656309bc0911aa784c5
itabdur <itabdurrahim22@gmail.com>
2022-03-26 21:51:29 +0600

    anote

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,28 +3,29 @@ import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 const Shop = () => {
-    const [products,setProduct]= useState([]);
-    const [cart,setCart]=useState(products);
- 
-    useEffect(()=>{
-    fetch('product.json')
-    .then(res=>res.json())
-    .then(data=>setProduct(data))
-    },[]);
+    const [products, setProduct] = useState([]);
+    const [cart, setCart] = useState(products);
+
+    useEffect(() => {
+        fetch('product.json')
+            .then(res => res.json())
+            .then(data => setProduct(data))
+    }, []);
 
 
 
-// console.log(cart)
-    const handleAddToCart =(selectedProduct)=>{
-       
-        const find = products.find(product=>product.id===selectedProduct);
-  
+    // console.log(cart)
+    const handleAddToCart = (selectedProduct) => {
+        const pd = products;
+
+        const find = products.find(product => product.id === selectedProduct);
+
         // console.log(find);
-  console.log(find.img);
-     const id = document.getElementById('id') ;
-     const div = document.createElement('div');
-     div.classList.add('dy-cart')
-     div.innerHTML=`
+        // console.log(find.img);
+        const id = document.getElementById('id');
+        const div = document.createElement('div');
+        div.classList.add('dy-cart')
+        div.innerHTML = `
      
      <img src="${find.img}" alt=""/>
      <h4>${find.name}</h4>
@@ -34,52 +35,72 @@ const Shop = () => {
      </div>
      
      `;
-     id.appendChild(div)
+        id.appendChild(div)
 
         
-  setCart(find)
-        
+
+        setCart(products)
+
     };
-    const choseAddToCurt= (product)=>{
-       const id =  Math.floor(Math.random() * 10) + 1;
-    let web = product.id;
-      web = id;
+    const choseAddToCurt = (productor) => {
+       
+
+        const ident = Math.floor(Math.random() * 5);
+        // consol>e.log(products);
+        const find = productor.find(product => product.id == ident);
+      const useId = document.getElementById('id');
+      const div = document.createElement('div')
+      div.classList.add('dy-cart')
+      useId.innerHTML=`
+      <img  style={{width:'25px'}} src="${find.img}" alt=""/>
+      <h4>${find.name}</h4>
+      `
+    //   console.log(find);
+    useId.appendChild(div)
+       
 
         
+  
+     
+    
+
     }
     // console.log(cart)
-   
-       
-    
+
+
+
     return (
         <div className='shop-container'>
             <div className='products-container'>
-                
-                   {
-                       products.map(product =><Product 
+
+                {
+                    products.map(product => <Product
                         product={product}
-                        key = {product.id}
+                        key={product.id}
                         handleAddToCart={handleAddToCart}
-                        >
+                    >
 
-                        </Product>)
-                   }
+                    </Product>)
+                }
             </div>
-        <div className="cart-container">
-            <div id='id' >
+            <div className="cart-container">
+                <div id='id' >
 
+                </div>
+                <div id='useId'>
+
+                </div>
+
+
+                <div>
+                    <div className='btn-tola'>
+
+                        <button onClick={() => choseAddToCurt(cart)} className='btn-1'> chose for one </button>
+                        <br />
+                        <button className='btn-1'>delter</button>
+                    </div>
+                </div>
             </div>
-        
-
-        <div>
-            <div className='btn-tola'>
-
-        <button onClick={()=>choseAddToCurt(cart)} className='btn-1'> chose for one </button>
-        <br />
-        <button className='btn-1'>delter</button>
-       </div>
-        </div>
-        </div>
         </div>
     );
 };
